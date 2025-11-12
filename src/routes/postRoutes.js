31b4348a588e6d103afd780fe4554df5b32a36df
eduardocardenasv27 <a00232432@tec.mx>
2025-11-11 20:34:52 -0700

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -10,5 +10,7 @@ const router = express.Router();
 
 router.post('/', createPost);
 router.get('/', getAllPosts);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
